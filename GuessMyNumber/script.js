12be4fc0ea3f5893105d05b8b0eba5ad2780265a
levvw <abdelraahmanmostafa@gmail.com>
2021-04-26 21:51:04 +0200

    control style through DOM

--- GuessMyNumber/script.js
+++ GuessMyNumber/script.js
@@ -8,11 +8,16 @@ document.querySelector(".number").textContent = secretNumber;
 
 document.querySelector(`.check`).addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
-  //   console.log(guess);
+
+  // when there is no guess
   if (!guess) {
     document.querySelector(".message").textContent = `No Number!`;
-  } else if (guess === secretNumber) {
+  }
+  // when player wins
+  else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "Correct Number!";
+    document.querySelector(`body`).style.backgroundColor = `#60b347`;
+    document.querySelector(`.number`).style.width = `30rem`;
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "Too High!";
