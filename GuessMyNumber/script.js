6ccc822404b1c5c7b121f3b5b5477ddf953b91fd
levvw <abdelraahmanmostafa@gmail.com>
2021-04-26 15:45:21 +0200

    made guess my number game functionalitty

--- GuessMyNumber/script.js
+++ GuessMyNumber/script.js
@@ -1,2 +1,35 @@
-'use strict';
-console.log(document.querySelector('.message').textContent);
+"use strict";
+
+// Random whole Number between 1 and 20
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+// Score variable starts at 20 and decrements with wrong guesses
+let score = 20;
+document.querySelector(".number").textContent = secretNumber;
+
+document.querySelector(`.check`).addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  //   console.log(guess);
+  if (!guess) {
+    document.querySelector(".message").textContent = `No Number!`;
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "Correct Number!";
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too High!";
+      score--;
+      document.querySelector(`.score`).textContent = score;
+    } else {
+      document.querySelector(`.score`).textContent = 0;
+      document.querySelector(".message").textContent = "You Lost The Game!";
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too Low!";
+      score--;
+      document.querySelector(`.score`).textContent = score;
+    } else {
+      document.querySelector(`.score`).textContent = 0;
+      document.querySelector(".message").textContent = "You Lost The Game!";
+    }
+  }
+});
