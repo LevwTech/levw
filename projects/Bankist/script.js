0ebb8ead8764e223176de8d47614b362f6b35413
levvw <abdelraahmanmostafa@gmail.com>
2021-07-29 21:58:28 +0200

    working on bankist app

--- projects/Bankist/script.js
+++ projects/Bankist/script.js
@@ -7,6 +7,7 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const header = document.querySelector(`.header`);
 
 const openModal = function () {
   modal.classList.remove('hidden');
@@ -29,3 +30,17 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+
+const cookieMsg = document.createElement(`div`);
+cookieMsg.innerHTML = `We use cookies to enhance performance <button class = " btn btn--close-cookie">Got it!</button>`
+header.append(cookieMsg);
+const closeBtn = document.querySelector(`.btn--close-cookie`);
+closeBtn.addEventListener('click', function(){
+  cookieMsg.remove();
+})
+
+// console.log(getComputedStyle(cookieMsg).width)
+
+
+
