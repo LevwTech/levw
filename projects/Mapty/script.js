9b022423bfe5677c0bfb8c24eb6345e32ac71e04
levvw <abdelraahmanmostafa@gmail.com>
2021-08-12 11:27:42 +0200

    added geolocation api and leaflet library

--- projects/Mapty/script.js
+++ projects/Mapty/script.js
@@ -10,3 +10,23 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+navigator.geolocation?.getCurrentPosition(function(position){
+const latitude = position.coords.latitude
+const longitude = position.coords.longitude
+const coords = [latitude,longitude]
+const googleMap = `https://www.google.com/maps/@${latitude},${longitude}`
+
+const map = L.map('map').setView(coords, 13);  // 'map'is a class an empty div in the html
+
+L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker(coords).addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
+
+}, function(){
+alert("couldnt get position, dont decline pls")
+});
