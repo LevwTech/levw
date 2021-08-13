0e866b6d75432e2a8ab36ad1fad07e193d1d5026
levvw <abdelraahmanmostafa@gmail.com>
2021-08-13 15:53:24 +0200

    implemented workout class

--- projects/Mapty/script.js
+++ projects/Mapty/script.js
@@ -11,10 +11,52 @@ const inputCadence = document.querySelector('.form__input--cadence');
 
 const inputElevation = document.querySelector('.form__input--elevation');
 
+
+class Workout {
+    date = new Date();
+    id = (Date.now() +'').slice(10);
+
+    constructor(coords,distance,duration) {
+        this.coords = coords;
+        this.distance = distance;  // in km
+        this.duration = duration; // in min
+    }
+
+}
+class Running extends Workout{
+    
+    constructor(coords,distance,duration,cadence) {
+        super(coords,distance,duration);
+        this.cadence = cadence
+        this.calcPace();
+    }
+
+    calcPace () {
+        this.pace = this.duration/this.distance;
+        return this.pace;
+
+    }
+
+}
+
+class Cycling extends Workout{
+
+    constructor(coords,distance,duration,elevationGain) {
+        super(coords,distance,duration);
+        this.elevationGain = this.elevationGain
+        this.calcSpeed();
+    }
+    calcSpeed() {
+        this.speed = this.distance/(this.duration/60);
+        return this.speed
+    }
+    
+}
 class App {
     
     #map;
     #mapEvent;
+    #workouts = [];
     constructor(){
         this._getPosition();
 
@@ -63,11 +105,33 @@ class App {
     }
 
     _newWorkout(e) {
+        const validInputs = (...inputs) => inputs.every(item=> Number.isFinite(item));
+        const isPositive =  (...inputs) => inputs.every(item=> item>0);
+        
+        const type = inputType.value;
+        const distance = Number(inputDistance.value)
+        const duration = Number(inputDuration.value)
+        const lat = this.#mapEvent.latlng.lat
+        const lng = this.#mapEvent.latlng.lng
+        let workout;
+
+        if (type === 'running') {
+            const cadence =  Number(inputCadence.value)
+            if(!validInputs(distance,duration,cadence) || !isPositive(distance,duration,cadence))
+             return alert("Should be a positive number 🙂"); 
+            
+             workout = new Running([lat,lng],distance,duration,cadence)  
+        }
+        if (type === 'cycling') {
+            const elevation =  Number(inputElevation.value)
+            if(!validInputs(distance,duration,elevation) || !isPositive(distance,duration)) 
+             return alert("Should be a positive number 🙂");   
+             workout = new Running([lat,lng],distance,duration,elevation) 
+        }
+        this.#workouts.push(workout);
 
         inputDistance.value = inputCadence.value = inputElevation.value = inputDuration.value = ''
         e.preventDefault();
-        const lat = this.#mapEvent.latlng.lat
-        const lng = this.#mapEvent.latlng.lng
         L.marker([lat,lng]).addTo(this.#map)
         .bindPopup(
             L.popup({
@@ -75,7 +139,7 @@ class App {
             minWidth: 100,
             autoClose: false,
             closeOnClick: false,
-            className: `running-popup`,
+            className: `${type}-popup`,
         })
         )
         .setPopupContent(`Workout`)
@@ -88,3 +152,8 @@ class App {
     const app = new App();
 
 
+// guard clause   if(opposite) return;
+// function takes any number of arguments and doubles them!  
+// ...numbers >> REST PATTERN  (take any number of arguments) (uses them as an array) used in func
+// const doubleNumbers = (...numbers) => numbers.map(number=>number*2);
+// console.log(doubleNumbers(1,2,3)) // [2,4,6]
